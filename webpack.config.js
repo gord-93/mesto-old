05a0215bfb490bc8e1b63b161102a01b24767880
Ivan Gordievsky <kos1993@icloud.com>
2020-10-27 02:30:38 +0300

    на проверку 8 ПР

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/pages/index.js',
+    output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    },
+
+    module: {
+        rules: [
+            {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                presets: ['@babel/preset-env']
+                    },
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2)$/i,
+                use: [
+                {
+                    loader: 'file-loader',
+                },
+                ],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                                importLoaders: 1,
+                        },
+                    },
+                    'postcss-loader'
+                    ],
+            },
+            ],
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin(),
+    ],
+
+    devtool: 'inline-source-map',
+};
